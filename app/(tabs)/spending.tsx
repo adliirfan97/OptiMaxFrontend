fca3f4f60a1d92c778df2a04b516c8e42af8fa5c
yawmoogle <daniel.lu.guanhua@gmail.com>
2025-02-04 15:57:22 +0800

    Correcting package dependencies and converting old placeholder pages to NativeWind

--- app/(tabs)/spending.tsx
+++ app/(tabs)/spending.tsx
@@ -1,31 +1,11 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Text, View } from 'react-native';
 
 export default function SpendingScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Spending</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/spending.tsx" />
-    </View>
+      <View className="flex-1 justify-center items-center bg-blue-500">
+        <Text className="text-white text-lg">Hello, NativeWind!</Text>
+        <Text className="text-white text-lg">This is the Spending Screen</Text>
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
