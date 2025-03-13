3f878c0d08dc58ff4dff8c7f619b46ba524a6d60
Eugene Leong <57002918+leonghh@users.noreply.github.com>
2025-03-13 10:32:25 +0800

    add more validation ad install graphql and apollo package

--- app/addCard.tsx
+++ app/addCard.tsx
@@ -1,16 +1,66 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import cardNumbers from '../constants/CardNumbers';
+
+const cardBrandImages: Record<string, any> = {
+    visa: require("../assets/cardBrands/visa-classic-svgrepo-com.png"),
+    mastercard: require("../assets/cardBrands/mastercard-full-svgrepo-com.png"),
+    americanExpress: require("../assets/cardBrands/american-express-logo-svgrepo-com.png"),
+};
 
 export default function AddCard({ navigation }) {
     const [cardName, setCardName] = useState('');
     const [cardNumber, setCardNumber] = useState('');
     const [expiryDate, setExpiryDate] = useState('');
     const [ccv, setCcv] = useState('');
+    const [cardBrand, setCardBrand] = useState('');
+    const [expiryError, setExpiryError] = useState('');
+    const [cardNameError, setCardNameError] = useState('');
+    const [cardNumberError, setCardNumberError] = useState('');
+    const [ccvError, setCcvError] = useState('');
 
     const handleAddCard = () => {
-        // Implementation of adding card logic
-        // For example, you can send the card details to your backend server or update the state
-        console.log({ cardName, cardNumber, expiryDate, ccv });
+        let hasError = false;
+
+        if (!cardName) {
+            setCardNameError('Card name is required');
+            hasError = true;
+        } else {
+            setCardNameError('');
+        }
+
+        if (!cardNumber) {
+            setCardNumberError('Card number is required');
+            hasError = true;
+        } else {
+            setCardNumberError('');
+        }
+
+        if (!expiryDate) {
+            setExpiryError('Expiry date is required');
+            hasError = true;
+        } else {
+            const error = checkExpiry(expiryDate);
+            if (error) {
+                setExpiryError(error);
+                hasError = true;
+            } else {
+                setExpiryError('');
+            }
+        }
+
+        if (!ccv) {
+            setCcvError('CCV is required');
+            hasError = true;
+        } else {
+            setCcvError('');
+        }
+
+        if (!hasError) {
+            // Implementation of adding card logic
+            // For example, you can send the card details to your backend server or update the state
+            console.log({ cardName, cardNumber, expiryDate, ccv, cardBrand });
+        }
     };
 
     return (
@@ -25,66 +75,130 @@ export default function AddCard({ navigation }) {
                     onChangeText={setCardName}
                     placeholder="Enter card name"
                 />
+                {cardNameError ? <Text style={styles.errorText}>{cardNameError}</Text> : null}
                 <Text style={styles.label}>Card Number</Text>
-                <TextInput
-                    style={styles.input}
-                    value={cardNumber}
-                    onChangeText={
-                        (text) => {
-                            if (text.length > 16) {
-                                setCardNumber(text.slice(0, 16));
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <TextInput
+                        style={[styles.input, { flex: 1 }]}
+                        value={cardNumber}
+                        onChangeText={(text) => {
+                            if (text.length > 19) {
+                                setCardNumber(text.slice(0, 19));
                             } else {
                                 setCardNumber(text);
                             }
-                        }
-                    }
-                    placeholder="Enter card number"
-                    keyboardType="numeric"
-                />
+                            const brand = getCardBrand(text);
+                            setCardBrand(brand);
+                        }}
+                        placeholder="Enter card number"
+                        keyboardType="numeric"
+                    />
+                    {cardBrand ? (
+                        <Image source={cardBrandImages[cardBrand]} style={styles.cardBrandImage} />
+                    ) : null}
+                </View>
+                {cardNumberError ? <Text style={styles.errorText}>{cardNumberError}</Text> : null}
                 <Text style={styles.label}>Expiry Date</Text>
                 <TextInput
-                    // const maxLength = 5;
                     style={styles.input}
                     value={expiryDate}
-                    onChangeText={
-                        (text) => {
-                            if (text.length === 2 && expiryDate.length === 1) {
-                                setExpiryDate(text + '/');
-                            } else if (text.length === 2 && expiryDate.length === 2) {
-                                setExpiryDate(text.slice(0, 1));
-                            } else if (text.length > 5) {
-                                setExpiryDate(text.slice(0, 5));
-                            }   else {
-                                setExpiryDate(text);
-                            }
+                    onChangeText={(text) => {
+                        if (text.length === 2 && expiryDate.length === 1) {
+                            setExpiryDate(text + '/');
+                        } else if (text.length === 2 && expiryDate.length === 2) {
+                            setExpiryDate(text.slice(0, 1));
+                        } else if (text.length > 5) {
+                            setExpiryDate(text.slice(0, 5));
+                        } else {
+                            setExpiryDate(text);
                         }
-                    }
+                    }}
                     placeholder="MM/YY"
-                    keyboardType='numeric'
+                    keyboardType="numeric"
                 />
+                {expiryError ? <Text style={styles.errorText}>{expiryError}</Text> : null}
                 <Text style={styles.label}>CCV</Text>
                 <TextInput
                     style={styles.input}
                     value={ccv}
-                    onChangeText={
-                        (text) => {
-                            if (text.length > 3) {
-                                setCcv(text.slice(0, 3));
-                            } else {
-                                setCcv(text);
-                            }
+                    onChangeText={(text) => {
+                        if (text.length > 3) {
+                            setCcv(text.slice(0, 3));
+                        } else {
+                            setCcv(text);
                         }
-                    }
+                    }}
                     placeholder="Enter CCV"
                     keyboardType="numeric"
                     secureTextEntry
                 />
-                <TouchableOpacity onPress={handleAddCard} className="bg-gray-300 p-4 rounded">
-                    <Text className="text-black font self-center">Add Card</Text>
+                {ccvError ? <Text style={styles.errorText}>{ccvError}</Text> : null}
+                <TouchableOpacity onPress={handleAddCard} style={styles.addButton}>
+                    <Text style={styles.addButtonText}>Add Card</Text>
                 </TouchableOpacity>
             </View>
         </View>
     );
+}
+
+function checkExpiry(expiryDate: string): string {
+    const [month, year] = expiryDate.split('/');
+    const currentYear = new Date().getFullYear() % 100; // Get last two digits of current year
+    const currentMonth = new Date().getMonth() + 1; // Get current month (0-based index)
+
+    if (parseInt(year) < currentYear || (parseInt(year) === currentYear && parseInt(month) < currentMonth)) {
+        return 'Card is expired';
+    }
+    return '';
+}
+
+function getCardBrand(cardNumber: string | number, validateLength = true): string {
+    let foundCardBrand = '';
+    let cardNum = cardNumber.toString().replace(/[-.\s]/g, '');
+
+    if (/^[0-9]+$/.test(cardNum.charAt(0))) {
+        cardNum = cardNum.replace(/[^0-9]/g, '0');
+        cardNum = cardNum.padEnd(6, '0');
+
+        const firstSixDigits = parseInt(cardNum.substring(0, 6), 10);
+        const cardNumberLength = cardNum.length;
+
+        for (const [brand, rows] of Object.entries(cardNumbers)) {
+            for (const [prefix, lengths] of Object.entries(rows)) {
+                let prefixMin = 0, prefixMax = 0;
+                if (prefix.includes('-')) {
+                    const [min, max] = prefix.split('-').map(p => parseInt(p.padEnd(6, '9'), 10));
+                    prefixMin = parseInt(min.toString().padEnd(6, '0'), 10);
+                    prefixMax = parseInt(max.toString().padEnd(6, '9'), 10);
+                } else {
+                    prefixMin = parseInt(prefix.padEnd(6, '0'), 10);
+                    prefixMax = parseInt(prefix.padEnd(6, '9'), 10);
+                }
+
+                const isValidPrefix = firstSixDigits >= prefixMin && firstSixDigits <= prefixMax;
+
+                if (isValidPrefix && !validateLength) {
+                    return brand;
+                }
+
+                if (isValidPrefix && validateLength) {
+                    for (const length of lengths) {
+                        let isValidLength = false;
+                        if (length.includes('-')) {
+                            const [minLength, maxLength] = length.split('-').map(Number);
+                            isValidLength = cardNumberLength >= minLength && cardNumberLength <= maxLength;
+                        } else {
+                            isValidLength = cardNumberLength === parseInt(length, 10);
+                        }
+                        if (isValidLength) {
+                            return brand;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return foundCardBrand;
 }
 
 const styles = StyleSheet.create({
@@ -114,13 +228,33 @@ const styles = StyleSheet.create({
     label: {
         fontSize: 16,
         marginBottom: 8,
+        marginTop: 12,
     },
     input: {
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
         borderRadius: 5,
-        marginBottom: 16,
+        marginBottom: 4,
         paddingHorizontal: 8,
+    },
+    cardBrandImage: {
+        width: 50,
+        height: 30,
+        marginBottom: 8,
+    },
+    errorText: {
+        color: 'red',
+    },
+    addButton: {
+        marginTop: 24,
+        backgroundColor: '#ddd',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    addButtonText: {
+        color: '#000',
+        fontWeight: 'bold',
     },
 });
